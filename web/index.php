686f41a0cc635181660379c6d5463c6ebdd6dc25
Gaku Yasui <pgmer123@gmail.com>
2015-11-30 15:57:12 +0000

    add some functions

--- web/index.php
+++ web/index.php
@@ -1,4 +1,19 @@
 <?php
 
 require '../bootstrap.php';
+$tmp = new Request;
+echo $tmp->getRequestUri();
+echo $tmp->isPost();
+echo $tmp->getHost();
+echo $tmp->isSsl();
+
+echo "<br />";
+echo $tmp->getBaseUrl() . "<br />";
+echo $tmp->getPathInfo();
+
+echo dirname(__FILE__).'/core';
+echo "<br />";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br />";
+echo dirname($_SERVER['SCRIPT_NAME']);
 
